33199b3d124fbe6601841a72538677f18d0bd07c
francisko92 <88834609+francisko92@users.noreply.github.com>
2026-08-16 09:47:44 +0200

    Actualitza el servei offline de la web

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const CACHE='vv-socis-v1';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./members.json','./logo-vikings.jpg','./icon.svg'];
+const CACHE='vv-socis-v2';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./members.json','./icon.svg','./privacy.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
